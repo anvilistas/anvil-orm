c40f4051d6628842fa4c8dd73be92ff3e0da2f62
Owen Campbell <owen.campbell@tanti.org.uk>
2020-10-06 15:34:02 +0100

    Remove redundnant import

--- server_code/persistence.py
+++ server_code/persistence.py
@@ -24,12 +24,12 @@
 import re
 
 import anvil.tables as tables
-import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
 from . import model
 
+__version__ = "0.1.0"
 camel_pattern = re.compile(r"(?<!^)(?=[A-Z])")
 
 
