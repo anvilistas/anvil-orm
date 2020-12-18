aa090e202638aac29ec51f24f9e5452d23a8beaa
Owen Campbell <owen.campbell@tanti.org.uk>
2020-12-18 12:10:05 +0000

    Bump version: 0.1.1 → 0.1.2

--- client_code/particles.py
+++ client_code/particles.py
@@ -26,7 +26,7 @@ import sys
 import anvil.server
 import anvil.users
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 class Attribute:
--- server_code/security.py
+++ server_code/security.py
@@ -24,7 +24,7 @@
 import anvil.server
 import anvil.users
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 def has_create_permission(class_name):
