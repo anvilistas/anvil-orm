d9d5d5efc5fd288640e3f50943c714de68d5b967
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2021-01-08 09:57:59 +0000

    Add cache and mixins

---
+++ client_code/client_lib/crud/cache.py
@@ -0,0 +1,42 @@
+# MIT License
+#
+# Copyright (c) 2020 Owen Campbell
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This software is published at https://github.com/meatballs/anvil-orm
+from app import model
+
+__version__ = "0.1.6"
+
+
+class Cache:
+    def __init__(self):
+        self._items = {}
+        
+    def refresh(self, model_name, max_depth=1, **search_args):
+        model_class = getattr(model, model_name)
+        self._items[model_name] = [o for o in model_class.search(**search_args)]
+        return self._items[model_name]
+      
+    def __getitem__(self, key):
+        return self._items[key]
+      
+    def __setitem__(self, key, value):
+        raise ValueError("You can only set the cache using the refresh method")
