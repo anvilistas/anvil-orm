6f8b7af9338e5ae559e39dc7507fa6525873162a
Owen Campbell <owen.campbell@tanti.org.uk>
2021-03-08 10:26:34 +0000

    Add missing version info

--- server_code/orm_server/__init__.py
+++ server_code/orm_server/__init__.py
@@ -1,15 +1,1 @@
-import anvil.server
-
-# This is a server package. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
-
+__version__ = "0.1.17"
